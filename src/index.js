6d66f1b1c45ed384285d37456b7a05ee236a1262
wen3298 <wzelayavega@gmail.com>
2024-06-03 16:08:36 -0600

    funcion agregar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-// Inserte el código aquí
+let btnagre = document.getElementById("addbtn")
+let cont = document.getElementById("contTareas")
 
+let addTask = ()=>{
+    let div = document.createElement("div")
+    let p = document.createElement("p")
+    let check = document.createElement("input")
+    check.type = "checkbox"
+    p.innerHTML= ( "  ")
+    div.appendChild(p)
+    div.appendChild(check)
+    cont.appendChild(div)
+}
+
+
+btnagre.addEventListener("click",addTask)
