64b1e3dd62644b289ccf45eb1ff6c84ad109ec73
wen3298 <wzelayavega@gmail.com>
2024-06-04 15:43:34 -0600

    funcion post

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const e = require("cors")
+
 let btnagre = document.getElementById("addbtn")
 let cont = document.getElementById("contTareas")
 
@@ -12,5 +14,43 @@ let addTask = ()=>{
     cont.appendChild(div)
 }
 
+
+//GET
+ async function getDatos() {
+    const respuesta = await fetch("")
+    const datos = respuesta.json()
+    datos.forEach(tarea=>{
+        
+        let p = document.createElement("p")
+    })
+    console.log(datos);    
+ }
+
+
+
+
+
+
+//POST
+async function postDatos() {
+    try {
+        let tarea = {
+            id:Date.now(),
+            titulo:"TAREA A",
+            estado: false
+        }
+        const respuesta = await fetch("http://localhost:3000/api/task",{
+            method: "POST",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              },
+              body: JSON.stringify(tarea)
+        })
+        const datos = await respuesta.json()
+        console.log(datos);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 btnagre.addEventListener("click",addTask)
