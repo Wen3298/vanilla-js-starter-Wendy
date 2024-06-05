0e52f461a3a9f56a604ccd2163a2c16f3c382345
wen3298 <wzelayavega@gmail.com>
2024-06-05 15:49:09 -0600

    fuciones

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-const e = require("cors")
-
-let btnagre = document.getElementById("addbtn")
-let cont = document.getElementById("contTareas")
-
-let addTask = ()=>{
-    let div = document.createElement("div")
-    let p = document.createElement("p")
-    let check = document.createElement("input")
-    check.type = "checkbox"
-    p.innerHTML= ( "  ")
-    div.appendChild(p)
-    div.appendChild(check)
-    cont.appendChild(div)
-}
+document.addEventListener("DOMContentLoaded",()=>{
+    let btnagre = document.getElementById("addbtn")
+    let tareas = document.getElementById("tareas")
 
 
 //GET
  async function getDatos() {
-    const respuesta = await fetch("")
-    const datos = respuesta.json()
+    try {
+        tareas.innerHTML=""
+        const respuesta = await fetch("http://localhost:3000/api/task")
+    const datos = await respuesta.json()
     datos.forEach(tarea=>{
-        
+        let div = document.createElement("div")
         let p = document.createElement("p")
+        let check = document.createElement("input")
+        check.type = "checkbox"
+        p.innerHTML=tarea.titulo
+        div.appendChild(p)
+        tareas.appendChild(div)
+        let btnEliminar = document.createElement("button")
+        btnEliminar.innerHTML="Eliminar"
+        p.appendChild(check)
+        p.appendChild(btnEliminar)
+        btnEliminar.addEventListener("click",()=>{
+            deleteTask(tarea.id)
+            
+        })
     })
-    console.log(datos);    
+    console.log(getDatos);  
+    } catch (error) {
+      console.error(error);  
+    }  
  }
-
-
-
-
-
 
 //POST
 async function postDatos() {
     try {
         let tarea = {
             id:Date.now(),
-            titulo:"TAREA A",
+            titulo:textoo.value,
             estado: false
         }
         const respuesta = await fetch("http://localhost:3000/api/task",{
@@ -48,9 +49,31 @@ async function postDatos() {
         })
         const datos = await respuesta.json()
         console.log(datos);
+        getDatos()
     } catch (error) {
         console.error(error);
     }
 }
+btnagre.addEventListener("click",postDatos)
 
-btnagre.addEventListener("click",addTask)
+//DELETE
+async function deleteTask(id) {
+    console.log("ENTRA");
+    try {
+        const respuesta = await fetch(`http://localhost:3000/api/task/${id}`,{
+            method: "DELETE",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              },
+        })
+        let datosSalida = respuesta.json()
+        console.log(datosSalida);
+        getDatos()
+    } catch (error) {
+        console.error(error);
+    }
+}
+})
+
+
+getDatos()
